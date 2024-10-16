c8eb2285fe7c1855628423b65dc89ed4b9b1aa06
Blackcat <akutoukuroneko@gmail.com>
2024-10-16 16:19:07 +0900

    comit test

---
+++ hello/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg':'これは、サンプルで作ったページです。',
+        'goto':'next',
+    }
+    return render (request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'これは、もう1つのページです。',
+        'goto':'index',
+    }
+    return render(request, 'hello/index.html', params)
+
+
+
