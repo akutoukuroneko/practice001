72172c6d83f5df46abd5e74daf7d12853d251764
Blackcat <akutoukuroneko@gmail.com>
2025-01-03 14:01:01 +0900

    HelloViewクラス作成

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,8 @@
+#django4でテキストと変更あり
 from django.urls import path
-from . import views
+from .views import HelloView
 
+#django4でテキストと変更あり
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(r'', HelloView.as_view(), name='index'),
 ]
